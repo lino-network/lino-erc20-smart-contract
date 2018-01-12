40349dfb4cf44b3ebe5bfeaa4829b5f9a7ad92d9
Ryan Li <liyihe96@gmail.com>
2018-01-11 20:02:59 -0800

    utils for event watching

---
+++ test/util.js
@@ -0,0 +1,21 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
+
+utils = {};
+utils.assertEvent = (contract, filter) => {
+  return new Promise((resolve, reject) => {
+    var event = contract[filter.event]();
+    event.watch();
+    event.get((error, logs) => {
+      var log = _.filter(logs, filter);
+      if (log) {
+        resolve(log);
+      } else {
+        throw Error('Failed to find filtered event for ' + filter.event);
+      }
+    });
+    event.stopWatching();
+  });
+};
+
+module.exports = utils;
