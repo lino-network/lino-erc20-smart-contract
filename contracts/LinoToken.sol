0f794b1311781a18a8512cb06881784d72b5e592
Ryan Li <liyihe96@gmail.com>
2018-01-11 21:58:26 -0800

    Style fix

--- contracts/LinoToken.sol
+++ contracts/LinoToken.sol
@@ -10,20 +10,20 @@ import "zeppelin-solidity/contracts/token/PausableToken.sol";
  * this contract.
  *
  */
- contract LinoToken is PausableToken {
+contract LinoToken is PausableToken {
 
-   string public constant name = 'LinoToken';               // Set the token name for display
-   string public constant symbol = 'LNO';                   // Set the token symbol for display
-   uint8 public constant decimals = 18;                     // Set the number of decimals for display
-   uint256 public constant INITIAL_SUPPLY = 10000000000;    // 10 billion LNO specified
+  string public constant name = "LinoToken";               // Set the token name for display
+  string public constant symbol = "LNO";                   // Set the token symbol for display
+  uint8 public constant decimals = 18;                     // Set the number of decimals for display
+  uint256 public constant INITIAL_SUPPLY = 10000000000;    // 10 billion LNO specified
 
-   /**
-    * @dev LinoToken Constructor
-    * Runs only on initial contract creation.
-    */
-    function LinoToken() public {
-      totalSupply = INITIAL_SUPPLY;                         // Set the total supply
-      balances[msg.sender] = INITIAL_SUPPLY;                // Tnitial tokens are assigned to creator
-      Transfer(0x0, msg.sender, INITIAL_SUPPLY);            // emit Transfer event
-    }
- }
+  /**
+  * @dev LinoToken Constructor
+  * Runs only on initial contract creation.
+  */
+  function LinoToken() public {
+    totalSupply = INITIAL_SUPPLY;                         // Set the total supply
+    balances[msg.sender] = INITIAL_SUPPLY;                // Tnitial tokens are assigned to creator
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);            // emit Transfer event
+  }
+}
